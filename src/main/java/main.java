7b9884c1a9bc400e89744da0d5fbdafd336ed1d5
yangoff <yangovip@gmail.com>
2017-05-02 00:33:07 -0300

    inicio

---
+++ src/main/java/main.java
@@ -0,0 +1,41 @@
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entity.Pessoa;
+
+/**
+ * Hello world!
+ *
+ */
+public class main
+{
+    public static void main(String [] args){
+        System.out.println("OlAR");
+
+        Pessoa p= new Pessoa();
+
+        p.setNome("gerivaldo");
+        p.setCpf(222222222);
+        p.setDtnasc(Calendar.getInstance().getTime());
+        p.setEmail("roselvmna@asodk.com");
+        p.setRg(2549925);
+        p.setTelefone("552525252");
+
+         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("franquia");
+        EntityManager manager = entityManagerFactory.createEntityManager();
+
+
+        try{
+            manager.getTransaction().begin();
+            manager.persist(p);
+            manager.getTransaction().commit();
+            manager.close();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
